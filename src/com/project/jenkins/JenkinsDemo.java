5710385e5991a1ae55605639a9ae5659669d9117
Pavani Naladala <pavani.naladala@gmail.com>
2016-06-19 10:43:57 -0700

    first commit
    first setup completed.

---
+++ src/com/project/jenkins/JenkinsDemo.java
@@ -0,0 +1,15 @@
+package com.project.jenkins;
+
+import org.testng.annotations.Test;
+
+public class JenkinsDemo {
+	
+	@Test
+	public void sampleJenkin()
+	{
+		System.out.println("Welcome to Jenkins");
+		System.out.println("Starting Jenkins");
+		System.out.println();
+	}
+
+}
